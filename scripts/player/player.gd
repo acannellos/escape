c41bc04e33095aecb6f078e449c57ac9f4bd7ca2
alcann <alex.cannellos@gmail.com>
2024-02-13 00:21:20 -0500

    dev player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody3D
 class_name Player
 
-@export var movement: PlayerMovementData
+@export var human_data: PlayerData
+@export var vampire_data: PlayerData
+
+var is_vampire: bool = false
+
+var data: PlayerData
 
 var speed: float = 5.0
 var acceleration: float = 0.25
@@ -16,14 +21,26 @@ var sensitivity: float = 0.001
 @onready var sub_viewport = $head/camera/SubViewportContainer/SubViewport
 @onready var sub_camera = $head/camera/SubViewportContainer/SubViewport/sub_camera
 
+@onready var collision = $CollisionShape3D
+
 @onready var footsteps = $footsteps
-@onready var floating = $floating
 
 var previously_floored: bool = false
 
 func _ready():
+	data = human_data
 	#Globals.player = self
 	sub_viewport.size = DisplayServer.window_get_size()
+	
+	set_data()
+
+func set_data():
+	footsteps.stream = data.footstep_sound
+	footsteps.pitch_scale = data.footstep_pitch
+	footsteps.play()
+	
+	sub_camera.color = data.skin_color
+	sub_camera.set_mat()
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -33,15 +50,34 @@ func _input(event):
 
 	if event.is_action_pressed("jump") and can_jump():
 		jump()
+	
+	if event.is_action_pressed("action_2"):
+		is_vampire = true
+		data = vampire_data
+		set_data()
+		#collision.disabled = true
+		footsteps.stream_paused = false
+	
+	if event.is_action_released("action_2"):
+		is_vampire = false
+		data = human_data
+		set_data()
+		#Audio.play("assets/sounds/player_land.wav")
+		#collision.disabled = false
 
 func _physics_process(delta):
+	
+	if footsteps.stream == null:
+		footsteps.set_stream(data.footstep_sound)
+		print("cool")
+	
 	handle_grounded(delta)
 	handle_movement(delta)
 	#sub_camera.global_transform = camera.global_transform
 	sub_camera.global_transform = head.global_transform
 	
 	if is_on_floor() and !previously_floored:
-		Audio.play("assets/sounds/land.wav")
+		Audio.play("assets/sounds/player_land.wav")
 		#camera.position.y = -0.1
 	
 	previously_floored = is_on_floor()
@@ -56,30 +92,22 @@ func _physics_process(delta):
 
 func handle_grounded(delta):	
 	if not is_on_floor():
-		velocity.y -= movement.gravity * delta
-		acceleration = movement.base_acceleration * movement.air_multiplier
+		velocity.y -= data.gravity * delta
+		acceleration = data.base_acceleration * data.air_multiplier
 		
-		footsteps.stream_paused = true
-
-		#floating.seek(0.0)
-		#floating.stream_paused = true
-	else:
-		jump_phase = movement.max_air_jumps
-		acceleration = movement.base_acceleration
-		if input_dir:
-			
-			footsteps.stream_paused = false
-			
-			#floating.stream_paused = false
-		else:
-			
+		if not is_vampire:
 			footsteps.stream_paused = true
-			
-			#floating.seek(0.0)
-			#floating.stream_paused = true
+	else:
+		jump_phase = data.max_air_jumps
+		acceleration = data.base_acceleration
+		if input_dir:
+			footsteps.stream_paused = false
+		else:
+			if not is_vampire:
+				footsteps.stream_paused = true
 
 func handle_movement(delta):
-	speed = movement.base_speed
+	speed = data.base_speed
 	input_dir = Input.get_vector("left", "right", "forward", "backward")
 	if input_dir:
 		direction = head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)
@@ -98,5 +126,5 @@ func can_jump() -> bool:
 	return (is_on_floor() or jump_phase > 0)
 
 func jump():
-	velocity.y = movement.jump_velocity
+	velocity.y = data.jump_velocity
 	jump_phase -= 1
